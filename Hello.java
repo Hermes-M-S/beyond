16e2a680b7ab68f94bb090e00d319586726e03cd
Hermes-M-S <1024hermes1024@gmail.com>
2020-01-28 03:24:57 +0100

    double a

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        int a =5;
+        double a =5;
         
         System.out.println("Hello, Java!!!!!!! AND GIT !!!");
 
