a9e5612fa28f8cbdaf3ecb6a9192ce9681873411
Hermes-M-S <1024hermes1024@gmail.com>
2020-01-28 03:33:26 +0100

    fail

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        int a = 5;
-        double c = 11;
+        double a = 5;
+        int c = 11;
         c = a+c;
         System.out.println(a);
         System.out.println("Hello, Java!!!!!!! AND GIT !!!");
