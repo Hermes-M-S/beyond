74905dcd824837f6cb1f7924ae6b81b292849438
Hermes-M-S <1024hermes1024@gmail.com>
2020-01-28 03:35:38 +0100

    set

--- Hello.java
+++ Hello.java
@@ -1,10 +1,17 @@
 public class Hello {
     public static void main(String[] args) {
         double a = 5;
-        int c = 11;
+        double c = 11;
         c = a+c;
+        c=Con.set();
         System.out.println(a);
         System.out.println("Hello, Java!!!!!!! AND GIT !!!");
 
+    }
+    
+    public class Con {
+        public static int set(){
+            return 10;
+        }
     }
 }
